6343da8b13b0297693c07d7b0b326bb248d5c7a2
unknown <Killich@.(none)>
2009-05-31 12:28:01 +0400

    basic migrations

---
+++ db/migrate/20090409192600_create_users.rb
@@ -0,0 +1,27 @@
+class CreateUsers < ActiveRecord::Migration
+  def self.up
+    create_table "users", :force => true do |t|
+      t.string    :login
+      t.string    :email
+      t.string    :crypted_password,          :limit => 40
+      t.string    :salt,                      :limit => 40
+      t.datetime  :created_at
+      t.datetime  :updated_at
+      t.string    :remember_token
+      t.datetime  :remember_token_expires_at
+
+      # Все дополнительные данные хранятся в профайле
+      t.string :name            # Имя пользователя
+      t.text :setting           # Набор различных настроек
+      
+      t.integer  "role_id"      # Ссылка на Роль пользователя в системе
+      t.integer  "profile_id"   # Ссылка на Профиль пользователя
+
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table "users"
+  end
+end
---
+++ db/migrate/20090501094751_create_roles.rb
@@ -0,0 +1,17 @@
+# Модель Роль
+# Позволяет организовать базовый функционал распеределния по ролям
+class CreateRoles < ActiveRecord::Migration
+  def self.up
+    create_table :roles do |t|
+      t.string    :title        # Название Роли
+      t.text      :description  # Текстовое описание Роли
+      t.text      :settings     # Настройки Роли
+      
+      t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :roles
+  end
+end
