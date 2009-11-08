c4c331063c2e234cd4a6f611bad6f1a029e79988
nginx <nginx@nginx001.firstvds.ru>
2009-11-09 00:26:33 +0300

    pages break fix

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,14 +3,14 @@ class Page < ActiveRecord::Base
   acts_as_nested_set :scope=>:user
   belongs_to :user
   
-  #validates_presence_of :user_id,      :message=>"Не определен идентификатор владельца страницы"
-  #validates_presence_of :zip,         :message=>"Не определен zip-идентификатор страницы"
+  validates_presence_of :user_id,      :message=>"Не определен идентификатор владельца страницы"
+  validates_presence_of :zip,         :message=>"Не определен zip-идентификатор страницы"
 
-  #validates_presence_of :author,      :message=>"Системное поле: Автор &mdash; не должно оставаться пустым"
-  #validates_presence_of :keywords,    :message=>"Системное поле: Ключевые слова &mdash; не должно оставаться пустым"
-  #validates_presence_of :description, :message=>"Системное поле: Описание &mdash; не должно оставаться пустым"
-  #validates_presence_of :copyright,   :message=>"Системное поле: Авторское право &mdash; не должно оставаться пустым"
-  #validates_presence_of :title,       :message=>"У страницы должен быть заголовок"
+  validates_presence_of :author,      :message=>"Системное поле: Автор &mdash; не должно оставаться пустым"
+  validates_presence_of :keywords,    :message=>"Системное поле: Ключевые слова &mdash; не должно оставаться пустым"
+  validates_presence_of :description, :message=>"Системное поле: Описание &mdash; не должно оставаться пустым"
+  validates_presence_of :copyright,   :message=>"Системное поле: Авторское право &mdash; не должно оставаться пустым"
+  validates_presence_of :title,       :message=>"У страницы должен быть заголовок"
   
   # ------------------------------------------------------------------  
   # Создать данному объекту zip код
