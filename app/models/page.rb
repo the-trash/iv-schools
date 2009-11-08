7946520f498ff47217a104dd1f44daf1e5154b2d
unknown <system-operator@.(none)>
2009-11-09 00:21:53 +0300

    'page

--- app/models/page.rb
+++ app/models/page.rb
@@ -3,19 +3,21 @@ class Page < ActiveRecord::Base
   acts_as_nested_set :scope=>:user
   belongs_to :user
   
-  #validates_presence_of :user_id,      :message=>"Не определен идентификатор владельца страницы"
-  #validates_presence_of :zip,         :message=>"Не определен zip-идентификатор страницы"
+  validates_presence_of :user_id,     :message=>"Не определен идентификатор владельца страницы"
+  validates_presence_of :zip,         :message=>"Не определен zip-идентификатор страницы"
 
-  #validates_presence_of :author,      :message=>"Системное поле: Автор &mdash; не должно оставаться пустым"
-  #validates_presence_of :keywords,    :message=>"Системное поле: Ключевые слова &mdash; не должно оставаться пустым"
-  #validates_presence_of :description, :message=>"Системное поле: Описание &mdash; не должно оставаться пустым"
-  #validates_presence_of :copyright,   :message=>"Системное поле: Авторское право &mdash; не должно оставаться пустым"
-  #validates_presence_of :title,       :message=>"У страницы должен быть заголовок"
+  validates_presence_of :author         #, :message=>"Системное поле: Автор &mdash; не должно оставаться пустым"
+  validates_presence_of :keywords       #, :message=>"Системное поле: Ключевые слова &mdash; не должно оставаться пустым"
+  validates_presence_of :description    #, :message=>"Системное поле: Описание &mdash; не должно оставаться пустым"
+  validates_presence_of :copyright      #, :message=>"Системное поле: Авторское право &mdash; не должно оставаться пустым"
+  validates_presence_of :title, :message=>"У страницы должен быть заголовок"
   
   # ------------------------------------------------------------------  
   # Создать данному объекту zip код
-  before_create :create_zip
+  before_validation_on_create :create_zip
   def create_zip
+    # Если zip уже установлен ранее
+    return unless (zip.nil? || zip.empty?)
     zip_code= "#{(1000..9999).to_a.rand}-#{(1000..9999).to_a.rand}-#{(1000..9999).to_a.rand}"
     while self.class.to_s.camelize.constantize.find_by_zip(zip_code)
       zip_code= "#{(1000..9999).to_a.rand}-#{(1000..9999).to_a.rand}-#{(1000..9999).to_a.rand}"
--- app/models/question.rb
+++ app/models/question.rb
@@ -59,8 +59,10 @@ class Question < ActiveRecord::Base
   
   # ------------------------------------------------------------------  
   # Создать данному объекту zip код
-  before_create :create_zip
+  before_validation_on_create :create_zip
   def create_zip
+    # Если zip уже установлен ранее
+    return unless (zip.nil? || zip.empty?)
     zip_code= "#{(1000..9999).to_a.rand}-#{(1000..9999).to_a.rand}-#{(1000..9999).to_a.rand}"
     while self.class.to_s.camelize.constantize.find_by_zip(zip_code)
       zip_code= "#{(1000..9999).to_a.rand}-#{(1000..9999).to_a.rand}-#{(1000..9999).to_a.rand}"
