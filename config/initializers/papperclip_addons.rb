78ef068c36b6ea6584fc85eaa48bc70f3d3f9473
unknown <user@.(none)>
2010-01-21 20:15:53 +0300

    image preview GREATE FIX

--- config/initializers/papperclip_addons.rb
+++ config/initializers/papperclip_addons.rb
@@ -1,7 +1,18 @@
+#Paperclip.options[:command_path] = "/usr/local/bin"
+#tom:~ tom$ export MAGICK_HOME="/opt/local/"
+#tom:~ tom$ export PATH="$MAGICK_HOME/bin:$PATH"
+#tom:~ tom$ export DYLD_LIBRARY_PATH="$MAGICK_HOME/lib"
+#/usr/local/ImageMagick-6.4.8
+#which convert
+#FREE BSD /usr/local/bin/convert
+# Ubuntu Paperclip.options[:command_path] = "/usr/bin/"
+#Paperclip.options[:command_path] = '/usr/local/bin/'
+#sudo apt-get install imagemagick ?
+
 module Paperclip
   module Interpolations
-    # Дополнения, позволяющие в путь сохранения изображений
-    # добавлять новые теги
+    # Р”РѕРїРѕР»РЅРµРЅРёСЏ, РїРѕР·РІРѕР»СЏСЋС‰РёРµ РІ РїСѓС‚СЊ СЃРѕС…СЂР°РЅРµРЅРёСЏ РёР·РѕР±СЂР°Р¶РµРЅРёР№
+    # РґРѕР±Р°РІР»СЏС‚СЊ РЅРѕРІС‹Рµ С‚РµРіРё
     
     # :url => '/uploads/:attachment/:login/:style.jpg'
     def login attachment, style
@@ -17,4 +28,12 @@ module Paperclip
       attachment.instance.zip
     end
   end
+end
+
+Paperclip::Attachment.class_eval do
+  def post_process_styles_with_validation
+    return unless instance.need_thumb? rescue nil
+    post_process_styles_without_validation
+  end
+  alias_method_chain :post_process_styles, :validation
 end
--- public/javascripts/jq/application.js
+++ public/javascripts/jq/application.js
@@ -1,10 +1,10 @@
 function show_editor_block(){
   $('#current_editor_bar').hide("blind", { direction: "vertical" }, 500);
-  $('#current_editor_block').delay(600).show("blind", { direction: "vertical" }, 700);
+  $('#current_editor_block').delay(500).show("blind", { direction: "vertical" }, 700);
 }
 function hide_editor_block(){
   $('#current_editor_block').hide("blind", { direction: "vertical" }, 700);
-  $('#current_editor_bar').delay(800).show("blind", { direction: "vertical" }, 500);
+  $('#current_editor_bar').delay(700).show("blind", { direction: "vertical" }, 500);
 }
 function show_add_file_form(){  
   $('#add_file_link').hide();
@@ -12,10 +12,10 @@ function show_add_file_form(){
 }
 function show_question_form(){
     $('#question_shower').hide("blind", { direction: "vertical" }, 300);
-    $('#question_block').delay(400).show("blind", { direction: "vertical" }, 500);
+    $('#question_block').delay(300).show("blind", { direction: "vertical" }, 500);
 }
 function hide_question_form(){
     $('#question_block').hide("blind", { direction: "vertical" }, 500);
-    $('#question_shower').delay(600).show("blind", { direction: "vertical" }, 500);
+    $('#question_shower').delay(500).show("blind", { direction: "vertical" }, 500);
 }
     
